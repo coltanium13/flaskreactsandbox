56e82a4bb1fcc0e21026eff19e9ef49f210a4c40
Colton <colton@Coltons-MBP.home>
2019-12-29 12:56:57 -0800

    front end swapped and running

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,29 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import axios from "axios";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Provider } from 'react-redux';
+
+import Posts from './components/Posts';
+import PostForm from './components/Postform';
+
+import store from './store';
 
 class App extends Component {
-    componentDidMount() {
-        const config = {
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            }
-        };
-        axios
-            .get("/api/test", config)
-            .then(res => {
-                console.log("response data: " + JSON.stringify(res.data));
-            })
-            .catch(err => {
-                console.log("error: " + err);
-            });
-    }
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload....
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://github.com/caseyr003/flask-react-template"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        View on Github
-                    </a>
-                </header>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
