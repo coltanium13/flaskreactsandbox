91b409874c90f4dde28511a28af8bd141e996cfe
Colton <colton@Coltons-MBP.home>
2019-12-30 16:16:55 -0800

    adding call with axios to flaskwith new frontend

--- frontend/src/components/Postform.js
+++ frontend/src/components/Postform.js
@@ -1,68 +1,84 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { useSelector, useDispatch } from "react-redux";
 import { createPost } from "../actions/postActions";
+import axios from "axios";
 
 function PostForm() {
-  const [newPost, setNewPost] = useState({
-    title: "",
-    body: ""
-  });
-
-  const dispatch = useDispatch();
-
-  const onChange = e => {
-    setNewPost({
-      ...newPost,
-      [e.target.name]: e.target.value
+    const [newPost, setNewPost] = useState({
+        title: "",
+        body: ""
     });
-  };
 
-  const onSubmit = e => {
-    e.preventDefault();
+    const dispatch = useDispatch();
 
-    const post = {
-      title: newPost.title,
-      body: newPost.body
+    useEffect(() => {
+        axios
+            .get("/api/test")
+            .then(res => {
+                console.log("response data: " + JSON.stringify(res.data));
+            })
+            .catch(err => {
+                console.log("error: " + err);
+            });
+    }, []);
+
+    const onChange = e => {
+        setNewPost({
+            ...newPost,
+            [e.target.name]: e.target.value
+        });
     };
 
-    dispatch(createPost(post));
+    const onSubmit = e => {
+        e.preventDefault();
 
-    setNewPost({
-      title: "",
-      body: ""
-    });
-  };
+        const post = {
+            title: newPost.title,
+            body: newPost.body
+        };
 
-  return (
-    <div>
-      <h1>Add Post</h1>
-      <form onSubmit={onSubmit}>
+        dispatch(createPost(post));
+
+        setNewPost({
+            title: "",
+            body: ""
+        });
+    };
+
+    return (
         <div>
-          <label>Title: </label>
-          <br />
-          <input
-            type="text"
-            name="title"
-            onChange={onChange}
-            value={newPost.title}
-          />
+            <h1>Add Post</h1>
+            <form onSubmit={onSubmit}>
+                <div>
+                    <label>Title: </label>
+                    <br />
+                    <input
+                        type="text"
+                        name="title"
+                        onChange={onChange}
+                        value={newPost.title}
+                    />
+                </div>
+                <br />
+                <div>
+                    <label>Body: </label>
+                    <br />
+                    <textarea
+                        name="body"
+                        onChange={onChange}
+                        value={newPost.body}
+                    />
+                </div>
+                <br />
+                <button type="submit">Submit</button>
+            </form>
         </div>
-        <br />
-        <div>
-          <label>Body: </label>
-          <br />
-          <textarea name="body" onChange={onChange} value={newPost.body} />
-        </div>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+    );
 }
 
 PostForm.propTypes = {
-  //createPost: PropTypes.func.isRequired
+    //createPost: PropTypes.func.isRequired
 };
 
 export default PostForm;
