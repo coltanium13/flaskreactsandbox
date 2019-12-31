d3cb4c085f3a28be6f036afb976fa2f220019792
Colton <colton@Coltons-MBP.home>
2019-12-30 18:04:06 -0800

    adding axios call in postActions

--- frontend/src/actions/postActions.js
+++ frontend/src/actions/postActions.js
@@ -1,40 +1,41 @@
 import { FETCH_POSTS, NEW_POST } from "./types";
 import _ from "lodash";
+import axios from "axios";
 
 export const fetchPosts = newItem => dispatch => {
-  console.log(JSON.stringify("newItem: " + JSON.stringify(newItem)));
-  fetch("https://jsonplaceholder.typicode.com/posts?_limit=10")
-    .then(res => res.json())
-    .then(posts => {
-      if (!_.isEmpty(newItem)) {
-        console.log("adding new item: " + JSON.stringify(newItem));
-        console.log("...posts: " + JSON.stringify(posts));
-        posts.unshift(newItem);
-      }
-      return posts;
-    })
-    .then(allPosts => {
-      console.log("allPosts" + JSON.stringify(allPosts));
-      dispatch({
-        type: FETCH_POSTS,
-        payload: allPosts
-      });
-    });
+    console.log(JSON.stringify("newItem: " + JSON.stringify(newItem)));
+    axios
+        .get("https://jsonplaceholder.typicode.com/posts?_limit=10")
+        .then(res => {
+            if (!_.isEmpty(newItem)) {
+                console.log("adding new item: " + JSON.stringify(newItem));
+                console.log("res.data: " + JSON.stringify(res.data));
+                res.data.unshift(newItem);
+            }
+            return res.data;
+        })
+        .then(allPosts => {
+            console.log("allPosts" + JSON.stringify(allPosts));
+            dispatch({
+                type: FETCH_POSTS,
+                payload: allPosts
+            });
+        });
 };
 
 export const createPost = postData => dispatch => {
-  fetch("https://jsonplaceholder.typicode.com/posts", {
-    method: "POST",
-    headers: {
-      "content-type": "application/json"
-    },
-    body: JSON.stringify(postData)
-  })
-    .then(res => res.json())
-    .then(post =>
-      dispatch({
-        type: NEW_POST,
-        payload: post
-      })
-    );
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(postData)
+    })
+        .then(res => res.json())
+        .then(post =>
+            dispatch({
+                type: NEW_POST,
+                payload: post
+            })
+        );
 };
